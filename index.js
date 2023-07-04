f65aefd6624e63258f6b0d6f6b1588e7ba3c4734
Akpo_Fure <okegbeakpofurekelvin@gmail.com>
2023-07-04 23:45:19 +0100

    Implemented a countdown feature

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-let textArea = document.getElementById('counter');
-let countDown = document.getElementById('click');
-let timer = document.getElementById('timer')
-console.log(timer)
-let count = 0
-let timerId;
-const clicker = () => { 
-    if(count === 0) textArea.textContent = ``
-    count++
-    textArea.textContent += `Clicked ${count} times  || `
-    clearTimeout(timerId)
-    timerId= setTimeout(function() {
-        count = 0
-        textArea.textContent = ``
-    }, 5000)
- }
-console.log(timerId)
+let textArea = document.getElementById('counter'); // Get the text area
+let countDown = document.getElementById('click'); // Get the countdown timer
+
+let count = 0; // Set the initial count value
+let timerId; // Set the initial timerId value
+let remainingTime = 5; // Set the initial countdown time in seconds
+
+const clicker = () => {
+    count++; // Increment the count
+    textArea.textContent += `Clicked ${count} times || `; // Update the text area
+    clearTimeout(timerId); // Clear the timeout
+
+    // Update the countdown timer
+    let seconds = remainingTime; // Set the initial countdown time in seconds
+    timerId = setInterval(() => { // Set the interval
+        seconds--; // Decrement the seconds
+        countDown.textContent = `Time remaining: ${seconds} seconds`; // Update the countdown timer
+
+        if (seconds === 0) { // If the seconds are 0
+            clearInterval(timerId); // Clear the interval
+            count = 0; // Reset the count
+            textArea.textContent = ''; // Reset the text area
+            countDown.textContent = 'Click the button to increase the counter'; // Reset the countdown timer
+        } 
+    }, 1000); // Set the interval to 1 second
+};
+
